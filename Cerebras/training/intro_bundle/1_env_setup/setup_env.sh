5f355fe862c7e95a3435618c55b6bc26129b95b7
Murali Emani <15612272+memani1@users.noreply.github.com>
2026-06-19 14:23:21 -0500

    Update environment setup for version 2.10

--- Cerebras/training/intro_bundle/1_env_setup/setup_env.sh
+++ Cerebras/training/intro_bundle/1_env_setup/setup_env.sh
@@ -3,7 +3,7 @@
 # NOTE: make sure you run this script as `source setup.sh`, not `sh setup.sh`
 # to make sure your environment is activated in one go.
 # If you run with sh, or for activating the env at a later time, just run
-# `source ../../venvs/r2.9/bin/activate`.
+# `source ../../venvs/r2.10/bin/activate`.
 
 # Needed to access the internet from the ALCF cluster.
 export HTTPS_PROXY='http://proxy.alcf.anl.gov:3128'                                                                                      
@@ -11,13 +11,13 @@ export HTTP_PROXY='http://proxy.alcf.anl.gov:3128'
 
 # Create and initialize new virtual environment.
 mkdir ../../venvs/
-python3.11 -m venv ../../venvs/r2.9
-source ../../venvs/r2.9/bin/activate
+python3.11 -m venv ../../venvs/r2.10
+source ../../venvs/r2.10/bin/activate
 # Doubles as a quick network connectivity check
 pip install --upgrade pip
 
 # Modelzoo setup
-git clone --branch Release_2.9.0 https://github.com/Cerebras/modelzoo.git ../modelzoo
+git clone --branch Release_2.10.0 https://github.com/Cerebras/modelzoo.git ../modelzoo
 # PEFT pin is only necessary for release 2.6-2.8
 # printf "\npeft==0.17.1" >> ../modelzoo/requirements.txt
 printf "\npsutil==7.1.3" >> ../modelzoo/requirements.txt
